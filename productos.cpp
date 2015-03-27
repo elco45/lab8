a3c2bab6c6585caad45cc9395695f028b048ef31
Elena Caballero <elena.caballero@unitec.edu>
2015-03-27 14:20:27 -0600

    productos

---
+++ productos.cpp
@@ -0,0 +1,21 @@
+#include "productos.h"
+#include "cliente.h"
+#include <string>
+#include <sstream>
+using namespace std;
+
+Productos::Productos(double volumen, double peso, Cliente nombre, double precio)
+		:volumen(volumen), peso(peso), nombre(nombre), precio(precio) {
+
+}
+Productos::Productos(Productos& prod):volumen(prod.volumen), peso(prod.peso), nombre(prod.nombre), precio(prod.precio) {
+
+}
+Productos::~Productos(){
+
+}
+string Productos::toString()const{
+	stringstream ss;
+	ss << "Producto \nVolumen: " << volumen << "\nPeso: " << peso << "\nNombre del Cliente: " << nombre.getNombre() << "\nPrecio: " << precio << "\n";
+	return ss.str();
+}
